23575763ae3926060b737c93090925312ee9b732
Jean Carlos Trejo <jeancarlostrejo19@gmail.com>
2024-09-25 19:12:23 -0400

    add tailwindcss

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Devstagram - @yield('title')</title>
+        @vite('./resources/css/app.css')
+    </head>
+    <body class="bg-gray-100">
+
+    </body>
+</html>
