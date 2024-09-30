66aa5a8afb01c33d8146c7f19308cfa9c3ff4dfb
Jean Carlos Trejo <jeancarlostrejo19@gmail.com>
2024-09-30 15:39:15 -0400

    change button logout for link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,9 +15,9 @@
                         <a href="#" class="font-bold text-gray-600 text-sm">Hola: <span class="font-normal">{{ auth()->user()->username }}</span></a>
                         <form action="{{ route('logout') }}" method="POST">
                             @csrf
-                            <button type="submit" class="font-bold uppercase text-gray-600 text-sm">
+                            <a  href="{{ route('logout') }}" class="font-bold uppercase text-gray-600 text-sm" onclick="event.preventDefault(); this.closest('form').submit()">
                                 Cerrar sesión
-                            </button>
+                            </a>
                         </form>
                     </nav>
                 @else
