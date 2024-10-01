8a26636521f677363ce4c6d44780939e243cdb87
Jean Carlos Trejo <jeancarlostrejo19@gmail.com>
2024-10-01 16:43:34 -0400

    add metho to store post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StorePostRequest;
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
@@ -27,6 +28,13 @@ class PostController extends Controller
 
     public function store(StorePostRequest $request)
     {
-        dd('guardando posts');
+        Post::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'image' => $request->image,
+            'user_id' => auth()->user()->id
+        ]);
+
+        return to_route('posts.index', auth()->user()->username);
     }
 }
