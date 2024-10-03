2d7c851542c3415c75be2e5698dab99de7fdbfc1
Jean Carlos Trejo <jeancarlostrejo19@gmail.com>
2024-10-03 13:40:08 -0400

    add link to the username of the person who published the post to go to the profile

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -54,7 +54,7 @@
             </div>
 
             <div>
-                <p class="font-bold">{{ $post->user->username }}</p>
+                <a href="{{ route('posts.index', $post->user->username) }}" class="font-bold">{{ $post->user->username }}</a>
                 <p class="text-sm text-gray-500"> {{ $post->created_at->diffForHumans() }}</p>
                 <p class="mt-5"> {{ $post->description }}</p>
             </div>
