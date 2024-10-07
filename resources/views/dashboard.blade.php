cc071638191d991d70d353f82f4dc7f1599f9ac7
Jean Carlos Trejo <jeancarlostrejo19@gmail.com>
2024-10-07 11:15:35 -0400

    add class component for list posts

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -65,22 +65,6 @@
 
     <section class="container mx-auto mt-10">
         <h2 class="text-4xl text-center font-black my-10">Publicaciones</h2>
-        @if($posts->count())
-            <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 p-3 sm:p-0">
-                @foreach ($posts as $post)
-                    <div>
-                        <a href="{{ route('posts.show', ['user' => $post->user, 'post' => $post]) }}">
-                            <img src="{{ Storage::url('posts/') . $post->image }}" alt="Imagen del post {{ $post->title }}">
-                        </a>
-                    </div>
-                @endforeach
-            </div>
-
-            <div class="tex-white mt-5 px-3 sm:p-0">
-                {{ $posts->links() }}
-            </div>
-        @else
-            <p class="text-gray-600 uppercase text-sm text-center font-bold">No hay posts</p>
-        @endif
+        <x-list-posts :posts="$posts" />
     </section>
 @endsection
