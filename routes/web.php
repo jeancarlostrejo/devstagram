25c7279452c03a912a6e6bb794e76a5aacdf24c8
Jean Carlos Trejo <jeancarlostrejo19@gmail.com>
2024-10-08 14:33:28 -0400

    add redirect if authenticated user visit login or register route

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,14 @@ use App\Http\Controllers\RegisterController;
 
 Route::get('/', HomeController::class)->name('home');
 
-Route::get('/register',[RegisterController::class, 'index'])->name('register');
-Route::post('/register',[RegisterController::class, 'store']);
-Route::get('/login', [LoginController::class, 'index'])->name('login');
-Route::post('/login', [LoginController::class, 'store']);
-Route::post('/logout',LogoutController::class)->name('logout');
+Route::middleware('guest')->group(function () {
+    Route::get('/register',[RegisterController::class, 'index'])->name('register');
+    Route::post('/register',[RegisterController::class, 'store']);
+    Route::get('/login', [LoginController::class, 'index'])->name('login');
+    Route::post('/login', [LoginController::class, 'store']);
+});
+
+Route::post('/logout',LogoutController::class)->name('logout')->middleware('auth');
 
 Route::get('/{user:username}', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
