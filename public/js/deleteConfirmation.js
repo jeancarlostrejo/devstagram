c26b1e0a9fa9aa8dc110aac463816cf243998514
Jean Carlos Trejo <jeancarlostrejo19@gmail.com>
2024-10-08 21:50:24 -0400

    fix error require the use of dropzone and post deletion form on the required pages and conditions

--- public/js/deleteConfirmation.js
+++ public/js/deleteConfirmation.js
@@ -1,21 +1,23 @@
 let form = document.querySelector("#form-delet");
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    Swal.fire({
-        title: "¿Estás seguro?",
-        text: "Se eliminará el registro",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        cancelButtonText: "No, cancelar",
-        confirmButtonText: "Sí, eliminar",
-        
-    }).then((result) => {
-        if (result.isConfirmed) {
-            e.target.submit();
-        }
+if(form){
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+    
+        Swal.fire({
+            title: "¿Estás seguro?",
+            text: "Se eliminará el registro",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            cancelButtonText: "No, cancelar",
+            confirmButtonText: "Sí, eliminar",
+            
+        }).then((result) => {
+            if (result.isConfirmed) {
+                e.target.submit();
+            }
+        });
     });
-});
+}
--- resources/js/app.js
+++ resources/js/app.js
@@ -3,31 +3,35 @@ import Dropzone from "dropzone";
 
 Dropzone.autoDiscover = false;
 
-let dropzone = new Dropzone(".dropzone", {
-    dictDefaultMessage: "Sube tu imagen aquí",
-    acceptedFiles: ".png,.jpg,.jpeg,.gif",
-    addRemoveLinks: true,
-    dictRemoveFile: "Borrar archivo",
-    maxFiles: 1,
-    uploadMultiple: false,
-    init: function () {
-        if (document.querySelector("[name='image']").value.trim()) {
-            const imagePosted = {};
-            imagePosted.size = 1111;
-            imagePosted.name = document.querySelector("[name='image']").value;
-            
-            this.options.addedfile.call(this, imagePosted);
-            this.options.thumbnail.call(this, imagePosted, `/storage/posts/${imagePosted.name}`)
+let pageNeedDropzone = document.querySelector('.dropzone')
 
-            imagePosted.previewElement.classList.add('dz-success', 'dz-complete')
+if(pageNeedDropzone){
+    let dropzone = new Dropzone(".dropzone", {
+        dictDefaultMessage: "Sube tu imagen aquí",
+        acceptedFiles: ".png,.jpg,.jpeg,.gif",
+        addRemoveLinks: true,
+        dictRemoveFile: "Borrar archivo",
+        maxFiles: 1,
+        uploadMultiple: false,
+        init: function () {
+            if (document.querySelector("[name='image']").value.trim()) {
+                const imagePosted = {};
+                imagePosted.size = 1111;
+                imagePosted.name = document.querySelector("[name='image']").value;
+                
+                this.options.addedfile.call(this, imagePosted);
+                this.options.thumbnail.call(this, imagePosted, `/storage/posts/${imagePosted.name}`)
+    
+                imagePosted.previewElement.classList.add('dz-success', 'dz-complete')
+            }
         }
-    }
-});
-
-dropzone.on('success', (file, response) =>{
-    document.querySelector("[name='image']").value = response.image;
-})
-
-dropzone.on('removedfile', (file) =>{
-    document.querySelector("[name='image']").value= ""
-})
+    });
+    
+    dropzone.on('success', (file, response) =>{
+        document.querySelector("[name='image']").value = response.image;
+    })
+    
+    dropzone.on('removedfile', (file) =>{
+        document.querySelector("[name='image']").value= ""
+    })
+}
